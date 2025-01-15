49c42623c90c5b63ece53e28fc7886f9d598e7fc
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2025-01-15 19:30:19 +0200

    basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnv } from './utils/getEnv.js';
+import { ENV_VARS } from './constants/env.js';
+
+const app = express();
+app.use(cors());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.get('/hello', (req, res, next) => {
+  return res.send('Hello world');
+});
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello world1' });
+});
+
+app.use('/hello', (err, req, res, next) => {
+  res.send(err.message);
+});
+
+const PORT = getEnv(ENV_VARS.PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
