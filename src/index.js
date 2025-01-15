fb9aa461b37cfe34e96f0c02b0dc5f59fbc36e23
Borys Meshkov <100526181+BorysMeshkov@users.noreply.github.com>
2025-01-15 21:16:24 +0200

    add get students routes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
-import { getEnv } from './utils/getEnv.js';
-import { ENV_VARS } from './constants/env.js';
+import { initMongoDBConnection } from './db/initiMongoDbConnection.js';
+import { startServer } from './server.js';
 
-const app = express();
-app.use(cors());
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.get('/hello', (req, res, next) => {
-  return res.send('Hello world');
-});
-
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello world1' });
-});
-
-app.use('/hello', (err, req, res, next) => {
-  res.send(err.message);
-});
-
-const PORT = getEnv(ENV_VARS.PORT);
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+await initMongoDBConnection();
+startServer();
